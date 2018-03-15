80bb00c21988c647cfa99be2a33234414dc5773d
Fragzowns <37031279+Fragzowns@users.noreply.github.com>
2018-03-15 19:47:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,17 +5,5 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
